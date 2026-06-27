eb5081b61f84752427d6989e4cc8898861027515
Brian Yuan <brian4685380@gmail.com>
2026-06-27 09:49:10 +0800

    Update OpenShelf to 0.4.3

--- Casks/openshelf.rb
+++ Casks/openshelf.rb
@@ -1,6 +1,6 @@
 cask "openshelf" do
-  version "0.4.2"
-  sha256 "4aa4258295099b130eddbda2a17912161fd2520bc93c044f941f61e97f753d1f"
+  version "0.4.3"
+  sha256 "6cb1f83b60d102642177b246436135a30582a9191c02080069686f49e4f025b7"
 
   url "https://github.com/brian4685380/OpenShelf/releases/download/v#{version}/OpenShelf-v#{version}-macOS.zip"
   name "OpenShelf"
